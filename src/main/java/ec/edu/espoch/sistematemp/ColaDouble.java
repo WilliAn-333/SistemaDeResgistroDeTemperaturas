15d3c818674b73506b04f661b50c128604a40b2e
WilliAn-333 <www.pegazo23@gmail.com>
2025-11-22 22:46:11 -0500

    creacion

---
+++ src/main/java/ec/edu/espoch/sistematemp/ColaDouble.java
@@ -0,0 +1,75 @@
+
+package ec.edu.espoch.sistematemp;
+
+
+public class ColaDouble {
+    private double[] datos;
+    private int frente;
+    private int fin;
+    private int tamaño;
+
+    public ColaDouble(int capacidad) {
+        datos = new double[capacidad];
+        frente = 0;
+        fin = -1;
+        tamaño = 0;
+    }
+
+    public boolean estaVacia() {
+        return tamaño == 0;
+    }
+
+    public boolean estaLlena() {
+        return tamaño == datos.length;
+    }
+
+    // Registrar nueva temperatura
+    public void encolar(double valor) {
+        if (estaLlena()) {
+            System.out.println("⚠ La cola está llena. No se puede registrar otra temperatura.");
+            return;
+        }
+        fin = (fin + 1) % datos.length;
+        datos[fin] = valor;
+        tamaño++;
+    }
+
+    // Procesar la temperatura más antigua
+    public double desencolar() {
+        if (estaVacia()) {
+            System.out.println("⚠ No hay temperaturas para procesar.");
+            return -1;
+        }
+        double temp = datos[frente];
+        frente = (frente + 1) % datos.length;
+        tamaño--;
+        return temp;
+    }
+
+    // Ver la siguiente temperatura a procesar
+    public double verFrente() {
+        if (estaVacia()) {
+            System.out.println("Cola vacía.");
+            return -1;
+        }
+        return datos[frente];
+    }
+
+    // Visualizar el registro completo
+    public void mostrar() {
+        if (estaVacia()) {
+            System.out.println(" Registro vacío.");
+            return;
+        }
+
+        System.out.print(" Registro actual de temperaturas: ");
+        int index = frente;
+
+        for (int i = 0; i < tamaño; i++) {
+            System.out.print(datos[index] + "°C  ");
+            index = (index + 1) % datos.length;
+        }
+        System.out.println();
+    }
+    
+}
---
+++ src/main/java/ec/edu/espoch/sistematemp/SistemaTemp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.sistematemp;
+
+/**
+ *
+ * @author USER
+ */
+public class SistemaTemp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
